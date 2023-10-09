177dc281de4c4160b4b809c9095f0d7769fd5e49
trwstin <68576751+trwstin@users.noreply.github.com>
2023-10-09 11:34:29 +0800

    update versions

--- common/src/main/java/io/github/polymeta/wondertrade/configuration/Pool.java
+++ common/src/main/java/io/github/polymeta/wondertrade/configuration/Pool.java
@@ -1,8 +1,5 @@
 package io.github.polymeta.wondertrade.configuration;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Pool {
     public Set<String> pokemon = new HashSet<>();
 }
